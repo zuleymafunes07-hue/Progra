ba4c681be0ebbfad3af42bcfe075d84d865843d7
rosmery funes <zuleymafunes07@gmai.com>
2026-07-27 17:41:46 -0600

    ejercicio de conversión de horas a minutos

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,1 +1,21 @@
-console.log("Esta es una pruba");
+//Elabore un programa que solicite una cantidad de horas y muestre su equivalencia en minutos.
+
+import readline from "node:readline";
+
+// 2-CREAMOS INTERFAZ
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+rl.question("Ingrese la cantidad de horas: " , function(horas){
+    
+    const minutos =  Number(horas) * 60;
+
+    console.log(`${horas} horas equivalentes a ${minutos} minutos`);
+
+    rl.close();
+
+
+})
+
